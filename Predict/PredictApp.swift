71a291cd0c31202753e06d312c941b677690d6fa
anorazhu <zhusm@bc.edu>
2024-09-16 16:39:12 -0400

    Initial Commit

---
+++ Predict/PredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictApp.swift
+//  Predict
+//
+//  Created by Anora Zhu on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PredictTests/PredictTests.swift
@@ -0,0 +1,17 @@
+//
+//  PredictTests.swift
+//  PredictTests
+//
+//  Created by Anora Zhu on 9/16/24.
+//
+
+import Testing
+@testable import Predict
+
+struct PredictTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
